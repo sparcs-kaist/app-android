04d09d7415269a867b9285f6e61d42160b6badbd
ametrine <yih2776@gmail.com>
2025-06-25 20:44:23 +0900

    feat: Types for Timetable

---
+++ app/src/main/java/com/example/soap/Models/Types/LectureType.kt
@@ -0,0 +1,10 @@
+package com.example.soap.Models.Types
+
+enum class LectureType{
+    BR, //Basic Required 기초 필수
+    BE, //Basic Elective 기초 선택
+    MR, //Major Required 전공 필수
+    ME, //Major Elective 전공 선택
+    HSE, //Humanities and Social Elective 인문 선택
+    ETC //기타
+}
