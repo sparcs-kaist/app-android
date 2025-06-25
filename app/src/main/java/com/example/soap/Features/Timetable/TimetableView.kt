2fb5051c8198ac553c707deade64a621e18ea4d4
ametrine <yih2776@gmail.com>
2025-06-25 23:04:42 +0900

    fix: UI change

--- app/src/main/java/com/example/soap/Features/Timetable/TimetableView.kt
+++ app/src/main/java/com/example/soap/Features/Timetable/TimetableView.kt
@@ -2,20 +2,40 @@ package com.example.soap.Features.Timetable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.soap.Features.PostCompose.PostComposeView
 import com.example.soap.Features.Timetable.Components.CompactTimetableSelector
+import com.example.soap.Features.Timetable.Components.TimetableGrid
 import com.example.soap.Features.Timetable.Components.TimetableSummary
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TimetableView(navController: NavController) {
+
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true,
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -24,7 +44,24 @@ fun TimetableView(navController: NavController) {
         Column(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)){
             CompactTimetableSelector()
 
+            TimetableGrid()
+
             TimetableSummary()
+
+            Button(
+                onClick = { showBottomSheet = true }
+            ) {
+                Text("임시 게시물 작성 버튼")
+            }
+            if(showBottomSheet){
+                ModalBottomSheet(
+                    onDismissRequest = { showBottomSheet = false },
+                    modifier = Modifier.fillMaxHeight(),
+                    sheetState = sheetState,
+                    containerColor = Color.White
+                ) { PostComposeView(viewModel()) }
+            }
+
         }
 
     }
