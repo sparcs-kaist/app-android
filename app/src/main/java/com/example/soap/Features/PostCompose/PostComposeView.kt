aa386fce71516da10ed3341170a33bd802e83674
ametrine <yih2776@gmail.com>
2025-06-25 23:00:58 +0900

    fix: UI change

--- app/src/main/java/com/example/soap/Features/PostCompose/PostComposeView.kt
+++ app/src/main/java/com/example/soap/Features/PostCompose/PostComposeView.kt
@@ -1,16 +1,22 @@
 package com.example.soap.Features.PostCompose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActionScope
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +29,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -32,6 +40,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.soap.Features.PostCompose.Components.FlairSelector
 import com.example.soap.Features.PostList.PostListViewModel
+import com.example.soap.R
 
 @Composable
 fun PostComposeView(postListViewModel: PostListViewModel) {
@@ -48,11 +57,17 @@ fun PostComposeView(postListViewModel: PostListViewModel) {
     var isNSFW by remember { mutableStateOf(false) }
     var isPolitical by remember { mutableStateOf(false) }
 
-    var isShowingCancelDialog by remember { mutableStateOf(false) }
+    val isDoneEnabled = title.isNotBlank() && description.isNotBlank()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
-    Column(Modifier.background(Color(0xFFF2F2F6))) {
+    Column(Modifier
+        .background(Color.White)
+        .fillMaxSize()
+        .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
         Box(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
         ) {
             Text(
                 text = "Write",
@@ -64,8 +79,14 @@ fun PostComposeView(postListViewModel: PostListViewModel) {
                 text = "Done",
                 style = MaterialTheme.typography.titleLarge,
                 fontWeight = FontWeight.Normal,
-                color = Color.Gray,
-                modifier = Modifier.align(Alignment.BottomEnd)
+                color = if (isDoneEnabled) Color(0xFF6157CD) else Color.Gray,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .clickable {
+                        if (isDoneEnabled) {
+                            //Todo : Post, Dismiss Button
+                        }
+                    }
             )
         }
         FlairSelector(postListViewModel)
@@ -86,6 +107,8 @@ fun PostComposeView(postListViewModel: PostListViewModel) {
 
         HorizontalDivider()
 
+        Spacer(Modifier.padding(4.dp))
+
         TextField(
             value = description,
             onValueChange = {description = it},
@@ -96,9 +119,63 @@ fun PostComposeView(postListViewModel: PostListViewModel) {
             focusRequester = descriptionFocusRequester
         )
 
-        TermsOfUseButton()
+        Box(Modifier.align(Alignment.End)){ TermsOfUseButton() }
+
+        Spacer(Modifier.weight(1f))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.background(Color.White)
+        ){
+            Icon(
+                painter = painterResource(R.drawable.add_photo_alternate),
+                contentDescription = "Add photo",
+                modifier = Modifier.clickable {  }
+            )
+
+            Spacer(Modifier.weight(1f))
+
+            CheckBoxText(
+                text = "Anonymous",
+                isChecked = writeAsAnonymous,
+                onCheckedChange = { writeAsAnonymous = !writeAsAnonymous }
+            )
+
+            CheckBoxText(
+                text = "NSFW",
+                isChecked = isNSFW,
+                onCheckedChange = { isNSFW = !isNSFW }
+            )
+
+            CheckBoxText(
+                text = "Political",
+                isChecked = isPolitical,
+                onCheckedChange = { isPolitical = !isPolitical}
+            )
+        }
     }
 
+}
+
+@Composable
+fun CheckBoxText(
+    text: String, 
+    isChecked: Boolean,
+    onCheckedChange:((Boolean) -> Unit)?
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = onCheckedChange,
+            colors = CheckboxDefaults.colors(Color.DarkGray)
+        )
+
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
 }
 
 @Composable
@@ -116,6 +193,7 @@ fun TextField(
         onValueChange = onValueChange,
         singleLine = singleLine,
         textStyle = textStyle.copy(color = Color.Black),
+        cursorBrush = SolidColor(Color(0xFF6157CD)),
         keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
         keyboardActions = KeyboardActions(
             onNext = nextFocus
