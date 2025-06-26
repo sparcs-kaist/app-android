72ef406160f92b67b05bfa8f9d42f06d4aa4e5c2
ametrine <yih2776@gmail.com>
2025-06-26 22:16:47 +0900

    design: Theme color update

--- app/src/main/java/com/example/soap/ui/theme/Color.kt
+++ app/src/main/java/com/example/soap/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.example.soap.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val theme_light_primary = Color(0xFF6157CD)
+val theme_light_background = Color(0xFFF2F2F6)
+val theme_light_onBackground = Color.Black
+val theme_light_surface = Color.White
+val theme_light_onSurface = Color.Gray
+val theme_light_surfaceDim = Color.LightGray
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val theme_dark_primary = Color(0xFFC5C0FF)
+val theme_dark_background = Color(0xFF262525)
+val theme_dark_onBackground = Color.White
+val theme_dark_surface = Color.Black
+val theme_dark_onSurface = Color.Gray
+val theme_dark_surfaceDim = Color.DarkGray
