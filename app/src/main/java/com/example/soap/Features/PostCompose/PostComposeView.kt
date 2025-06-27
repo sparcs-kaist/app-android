254481798146e846a151547a2b57d31124f26ae1
ametrine <yih2776@gmail.com>
2025-06-28 04:29:42 +0900

    design: Add Animation

--- app/src/main/java/com/example/soap/Features/PostCompose/PostComposeView.kt
+++ app/src/main/java/com/example/soap/Features/PostCompose/PostComposeView.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalMinimumInteractiveComponentSize
