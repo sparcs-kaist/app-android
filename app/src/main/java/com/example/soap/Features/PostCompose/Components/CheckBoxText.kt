b6219ac44c6c75e93e9b0b87e6c3d74e4c443acb
ametrine <yih2776@gmail.com>
2025-06-28 23:24:47 +0900

    fix: post compose components moved

---
+++ app/src/main/java/com/example/soap/Features/PostCompose/Components/CheckBoxText.kt
@@ -0,0 +1,54 @@
+package com.example.soap.Features.PostCompose.Components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.LocalMinimumInteractiveComponentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.soap.ui.theme.SoapTheme
+
+
+@Composable
+fun CheckBoxText(
+    text: String,
+    isChecked: Boolean,
+    onCheckedChange:((Boolean) -> Unit)?
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides 4.dp) {
+
+            Checkbox(
+                checked = isChecked,
+                onCheckedChange = onCheckedChange,
+                colors = CheckboxDefaults.colors(MaterialTheme.colorScheme.outline),
+                modifier = Modifier.padding(8.dp)
+            )
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun Preview(){
+    SoapTheme {
+        CheckBoxText(
+            text = "Text",
+            isChecked = false,
+            onCheckedChange = {}
+    )
+    }
+}
