7b489f7e254f49f45bd3b568db39858271cf4126
Inha Yoon <yih2776@gmail.com>
2025-07-02 01:07:45 +0900

    feat: boardlistview and components

--- app/src/main/java/com/example/soap/Features/BoardList/BoardListView.kt
+++ app/src/main/java/com/example/soap/Features/BoardList/BoardListView.kt
@@ -13,12 +13,17 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.soap.Features.BoardList.Components.BoardList
+import com.example.soap.Features.BoardList.Components.BoardListSectionItem
 import com.example.soap.Features.NavigationBar.AppBar
 import com.example.soap.Features.NavigationBar.AppDownBar
 import com.example.soap.Features.NavigationBar.Channel
+import com.example.soap.R
+import com.example.soap.ui.theme.SoapTheme
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,13 +57,61 @@ fun BoardListView(navController: NavController) {
                 .verticalScroll(scrollState)
                 .padding(innerPadding)
         ) {
+            BoardList(
+                title = "Notice",
+                icon = painterResource(R.drawable.round_notifications_active),
+                sections = listOf{
+                    BoardListSectionItem(text = "Portal Notice", onClick = {})
+                    BoardListSectionItem(text = "Staff Notice", onClick = {})
+                    BoardListSectionItem(text = "Facility Notice", onClick = {})
+                    BoardListSectionItem(text = "External Company Advertisement", onClick = {})
+                }
+            )
 
+            BoardList(
+                title = "Talk",
+                icon = painterResource(R.drawable.round_chat),
+                sections = listOf{
+                    BoardListSectionItem(text = "General", onClick = {})
+                }
+            )
+
+            BoardList(
+                title = "Organisations and Clubs",
+                icon = painterResource(R.drawable.group),
+                sections = listOf{
+                    BoardListSectionItem(text = "Students Group", onClick = {})
+                    BoardListSectionItem(text = "Club", onClick = {})
+                }
+            )
+
+            BoardList(
+                title = "Trade",
+                icon = painterResource(R.drawable.baseline_local_offer),
+                sections = listOf{
+                    BoardListSectionItem(text = "Wanted", onClick = {})
+                    BoardListSectionItem(text = "Market", onClick = {})
+                    BoardListSectionItem(text = "Real Estate", onClick = {})
+                }
+            )
+
+            BoardList(
+                title = "Communication",
+                icon = painterResource(R.drawable.baseline_drafts),
+                sections = listOf{
+                    BoardListSectionItem(text = "Facility Feedback", onClick = {})
+                    BoardListSectionItem(text = "Ara Feedback", onClick = {})
+                    BoardListSectionItem(text = "Messages to the School", onClick = {})
+                    BoardListSectionItem(text = "KAIST News", onClick = {})
+                }
+            )
         }
     }
 }
 
+
 @Composable
 @Preview
 private fun Preview() {
-    BoardListView(navController = rememberNavController())
+    SoapTheme{ BoardListView(navController = rememberNavController()) }
 }
---
+++ app/src/main/java/com/example/soap/Features/BoardList/Components/BoardList.kt
@@ -0,0 +1,84 @@
+package com.example.soap.Features.BoardList.Components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.soap.R
+import com.example.soap.ui.theme.SoapTheme
+
+@Composable
+fun BoardList(
+    title: String,
+    icon: Painter,
+    sections: List<@Composable () -> Unit>
+){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(4.dp)
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onBackground
+            )
+
+            Spacer(Modifier.padding(4.dp))
+
+            Text(
+                text = title,
+                fontWeight = FontWeight.SemiBold,
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+        Card(
+            colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surface),
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                sections.forEachIndexed { index, section ->
+                    section()
+                    if (index < sections.lastIndex) {
+                        HorizontalDivider(
+                            thickness = 0.7.dp,
+                            color = MaterialTheme.colorScheme.outlineVariant
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun Preview(){
+    SoapTheme{ BoardList(title = "title", icon = painterResource(R.drawable.baseline_drafts), listOf{}) }
+}
