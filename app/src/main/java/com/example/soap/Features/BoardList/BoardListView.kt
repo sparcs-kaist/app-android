69b19d8ec2d6077d90d77c92763395b9f17eed20
Inha Yoon <yih2776@gmail.com>
2025-07-02 00:19:04 +0900

    fix: navigationbar

--- app/src/main/java/com/example/soap/Features/BoardList/BoardListView.kt
+++ app/src/main/java/com/example/soap/Features/BoardList/BoardListView.kt
@@ -1,8 +1,64 @@
 package com.example.soap.Features.BoardList
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.soap.Features.NavigationBar.AppBar
+import com.example.soap.Features.NavigationBar.AppDownBar
+import com.example.soap.Features.NavigationBar.Channel
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BoardListView(navController: NavController) {
+
+    val scrollState = rememberScrollState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        topBar = {
+            AppBar(
+                currentScreen = Channel.Boards,
+                scrollBehavior = scrollBehavior
+            )
+                 },
+
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        bottomBar = {
+            AppDownBar(
+                navController = navController,
+                currentScreen = Channel.Boards
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .verticalScroll(scrollState)
+                .padding(innerPadding)
+        ) {
+
+        }
+    }
+}
 
 @Composable
-fun BoardListView() {
+@Preview
+private fun Preview() {
+    BoardListView(navController = rememberNavController())
 }
--- app/src/main/java/com/example/soap/Features/Main/MainActivity.kt
+++ app/src/main/java/com/example/soap/Features/Main/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.soap.Features.NavigationBar.NavigationBar
+import com.example.soap.Features.NavigationBar.MainTabBar
 import com.example.soap.ui.theme.SoapTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SoapTheme {
-                NavigationBar()
+                MainTabBar()
             }
         }
     }
