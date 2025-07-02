c6723f296be5a68574b803d14394cb586bd76c66
Inha Yoon <yih2776@gmail.com>
2025-07-02 11:33:47 +0900

    feat: localizedstring

---
+++ app/src/main/java/com/example/soap/Utilities/Helpers/LocalizedString.kt
@@ -0,0 +1,18 @@
+package com.example.soap.Utilities.Helpers
+
+import java.util.Locale
+
+data class LocalizedString(
+    private val translations: Map<String, String>
+) {
+    fun localized(languageCode: String? = null): String {
+        val localeLanguageCode = languageCode
+            ?: Locale.getDefault().language
+            ?: "ko"
+        return translations[localeLanguageCode] ?: translations["ko"] ?: "Untitled"
+    }
+
+    override fun toString(): String {
+        return localized()
+    }
+}
