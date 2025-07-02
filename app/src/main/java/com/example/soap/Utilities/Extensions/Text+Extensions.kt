d50a5013a91d96e1b303ee80f76889b99aa36038
Inha Yoon <yih2776@gmail.com>
2025-07-02 18:43:03 +0900

    feat: Extensions

---
+++ app/src/main/java/com/example/soap/Utilities/Extensions/Text+Extensions.kt
@@ -0,0 +1,31 @@
+package com.example.soap.Utilities.Extensions
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.TextUnit
+import com.example.soap.Utilities.Helpers.LocalizedString
+
+@Composable
+fun LocalizedText(
+    text: LocalizedString,
+    style: TextStyle = TextStyle.Default,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontFamily: FontFamily? = null,
+    maxLines: Int = Int.MAX_VALUE,
+) {
+    Text(
+        text = text.localized(),
+        style = style,
+        modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontFamily = fontFamily,
+        maxLines = maxLines
+    )
+}
