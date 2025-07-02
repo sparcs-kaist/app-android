3c3ad038737c0ff74db8837db20b89ce4a580f85
Inha Yoon <yih2776@gmail.com>
2025-07-02 11:33:57 +0900

    fix: mocklist

--- app/src/main/java/com/example/soap/Features/PostList/PostListViewModel.kt
+++ app/src/main/java/com/example/soap/Features/PostList/PostListViewModel.kt
@@ -2,10 +2,11 @@ package com.example.soap.Features.PostList
 
 import androidx.lifecycle.ViewModel
 import com.example.soap.Models.Post
+import com.example.soap.Utilities.Mocks.mockList
 
 class PostListViewModel : ViewModel(){
 
-    var postList: List<Post> = Post.mocklist()
+    var postList: List<Post> = Post.mockList()
 
     var flairList: List<String> = listOf(
         "SPPANGS",
