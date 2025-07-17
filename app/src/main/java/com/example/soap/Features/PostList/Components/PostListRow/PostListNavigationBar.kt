d7d6df1361b1df12ff733572a67f7524bb74956c
Inha Yoon <yih2776@gmail.com>
2025-07-18 03:56:24 +0900

    fix: navigation bar center aligned

--- app/src/main/java/com/example/soap/Features/PostList/Components/PostListRow/PostListNavigationBar.kt
+++ app/src/main/java/com/example/soap/Features/PostList/Components/PostListRow/PostListNavigationBar.kt
@@ -2,12 +2,11 @@ package com.example.soap.Features.PostList.Components.PostListRow
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -25,11 +23,10 @@ import com.example.soap.R
 import com.example.soap.ui.theme.SoapTheme
 import com.example.soap.ui.theme.soapColors
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BoardNavigationBar(navController: NavController) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         navigationIcon = {
             Row(
                 modifier = Modifier.clickable { navController.navigate(Channel.Start.name) },
@@ -51,15 +48,13 @@ fun BoardNavigationBar(navController: NavController) {
             Text(
                 text = stringResource(R.string.trending_board),
                 style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center
+                fontWeight = FontWeight.Bold
             )
         },
         actions = {
             SearchButton()
         },
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
             containerColor = MaterialTheme.soapColors.background
         )
     )
