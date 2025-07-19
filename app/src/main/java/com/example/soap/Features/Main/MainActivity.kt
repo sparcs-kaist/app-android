e912590012119b67129e9b9a182001fdb466c73e
Inha Yoon <yih2776@gmail.com>
2025-07-19 09:43:47 +0900

    feat: hilt plugin add

--- app/src/main/java/com/example/soap/Features/Main/MainActivity.kt
+++ app/src/main/java/com/example/soap/Features/Main/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.soap.Features.NavigationBar.MainTabBar
 import com.example.soap.ui.theme.SoapTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
