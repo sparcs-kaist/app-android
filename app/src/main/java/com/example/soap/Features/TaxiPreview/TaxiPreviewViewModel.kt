5b7e6c78f2a683126f9aa9ca48e9918186463c35
Inha Yoon <yih2776@gmail.com>
2025-07-29 04:47:14 +0900

    feat: TimePreviewViewmodel

---
+++ app/src/main/java/com/example/soap/Features/TaxiPreview/TaxiPreviewViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.soap.Features.TaxiPreview
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.soap.Domain.Models.Taxi.TaxiParticipant
+import com.example.soap.Domain.Models.Taxi.TaxiUser
+import com.example.soap.Domain.Repositories.TaxiRoomRepository
+import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class TaxiPreviewViewModel @Inject constructor(
+    private val taxiRoomRepository: TaxiRoomRepository,
+//    private val userUseCase: UserUseCase
+) : ViewModel() {
+
+    // MARK: - Properties
+    private val _taxiUser = MutableStateFlow<TaxiUser?>(null)
+    val taxiUser: StateFlow<TaxiUser?> = _taxiUser
+
+    // MARK: - Init
+//    init {
+//        fetchTaxiUser()
+//    }
+
+    // MARK: - Logic
+    fun isJoined(participants: List<TaxiParticipant>): Boolean {
+        return participants.any { it.id == _taxiUser.value?.oid }
+    }
+
+    fun calculateRoutePoints(
+        source: LatLng,
+        destination: LatLng
+    ): List<LatLng> {
+        return listOf(source, destination)
+    }
+
+    fun joinRoom(id: String) {
+        viewModelScope.launch {
+            try {
+                taxiRoomRepository.joinRoom(id)
+            } catch (e: Exception) {
+
+            }
+        }
+    }
+
+
+//    private fun fetchTaxiUser() {
+//        viewModelScope.launch {
+//            _taxiUser.value = userUseCase.getTaxiUser()
+//        }
+//
+}
