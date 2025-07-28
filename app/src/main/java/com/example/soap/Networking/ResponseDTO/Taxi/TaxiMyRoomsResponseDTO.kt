faf56773f864b96111c5809edbe59c50dda3d455
Inha Yoon <yih2776@gmail.com>
2025-07-29 04:58:16 +0900

    feat: Taxi DTO

---
+++ app/src/main/java/com/example/soap/Networking/ResponseDTO/Taxi/TaxiMyRoomsResponseDTO.kt
@@ -0,0 +1,11 @@
+package com.example.soap.Networking.ResponseDTO.Taxi
+
+import com.google.gson.annotations.SerializedName
+
+data class TaxiMyRoomsResponseDTO(
+    @SerializedName("ongoing")
+    val onGoing: List<TaxiRoomDTO>,
+
+    @SerializedName("done")
+    val done: List<TaxiRoomDTO>
+)
---
+++ app/src/main/java/com/example/soap/Networking/ResponseDTO/Taxi/TaxiRoomDTO.kt
@@ -0,0 +1,57 @@
+package com.example.soap.Networking.ResponseDTO.Taxi
+
+import com.example.soap.Domain.Models.Taxi.TaxiRoom
+import com.example.soap.Shared.Extensions.toDate
+import com.google.gson.annotations.SerializedName
+import java.util.Date
+
+data class TaxiRoomDTO(
+    @SerializedName("_id")
+    val id: String,
+
+    @SerializedName("name")
+    val name: String,
+
+    @SerializedName("from")
+    val from: TaxiLocationDTO,
+
+    @SerializedName("to")
+    val to: TaxiLocationDTO,
+
+    @SerializedName("time")
+    val time: String,
+
+    @SerializedName("part")
+    val participants: List<TaxiParticipantDTO>,
+
+    @SerializedName("madeat")
+    val madeAt: String,
+
+    @SerializedName("maxPartLength")
+    val maxParticipants: Int,
+
+    @SerializedName("settlementTotal")
+    val settlementTotal: Int?,
+
+    @SerializedName("isDeparted")
+    val isDeparted: Boolean,
+
+    @SerializedName("isOver")
+    val isOver: Boolean?
+) {
+    fun toModel(): TaxiRoom {
+        return TaxiRoom(
+            id = id,
+            title = name,
+            source = from.toModel(),
+            destination = to.toModel(),
+            departAt = time.toDate() ?: Date(),
+            participants = participants.map { it.toModel() },
+            madeAt = madeAt.toDate() ?: Date(),
+            capacity = maxParticipants,
+            settlementTotal = settlementTotal,
+            isDeparted = isDeparted,
+            isOver = isOver
+        )
+    }
+}
