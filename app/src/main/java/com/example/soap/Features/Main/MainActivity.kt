c12df814e1900ace2a93a204201016a0d1e1c9bd
Inha Yoon <yih2776@gmail.com>
2025-08-25 14:03:13 +0900

    add signin feature

--- app/src/main/java/com/example/soap/Features/Main/MainActivity.kt
+++ app/src/main/java/com/example/soap/Features/Main/MainActivity.kt
@@ -4,19 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.example.soap.Features.NavigationBar.MainTabBar
+import com.example.soap.Features.SignIn.SignInView
 import com.example.soap.ui.theme.Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             Theme {
-                MainTabBar()
+                val isAuthenticated by viewModel.isAuthenticated.collectAsState()
+                val isLoading by viewModel.isLoading.collectAsState()
+
+                when {
+                    isLoading -> {}
+                    isAuthenticated == true -> {
+                        MainTabBar()
+                    }
+
+                    else -> {
+                        SignInView()
+                    }
+                }
             }
         }
     }
+
 }
