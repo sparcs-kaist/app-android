3a78b0863ba13f0439a2006eaf09a1b5dd9830e1
Inha Yoon <yih2776@gmail.com>
2025-08-31 05:07:48 +0900

    settings feature

--- app/src/main/java/com/example/soap/Features/Settings/Ara/AraBlockedUsersView.kt
+++ app/src/main/java/com/example/soap/Features/Settings/Ara/AraBlockedUsersView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,6 +29,7 @@ import com.example.soap.Features.NavigationBar.Channel
 import com.example.soap.Features.Settings.Components.SettingsViewNavigationBar
 import com.example.soap.ui.theme.Theme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AraBlockedUsersView(
     initialBlockedUsers: List<String>,
