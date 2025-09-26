cb6cb92ed8042b7397ebdcabab5ca5c954a4564c
Inha Yoon <yih2776@gmail.com>
2025-09-27 08:11:05 +0900

    fix: add repo

--- app/src/main/java/com/example/soap/Domain/Repositories/Feed/FeedImageRepository.kt
+++ app/src/main/java/com/example/soap/Domain/Repositories/Feed/FeedImageRepository.kt
@@ -1,9 +1,9 @@
 package com.example.soap.Domain.Repositories.Feed
 
-import android.graphics.Bitmap
 import com.example.soap.Domain.Models.Feed.FeedImage
 import com.example.soap.Features.FeedPostCompose.FeedPostPhotoItem
 import com.example.soap.Networking.RetrofitAPI.Feed.FeedImageApi
+import com.example.soap.Shared.Extensions.compressForUpload
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -17,20 +17,19 @@ class FeedImageRepository(
 ): FeedImageRepositoryProtocol {
 
     override suspend fun uploadPostImage(item: FeedPostPhotoItem): FeedImage {
-//        val imageData = item.image.compressForUpload(10.0)
-//            ?: throw IllegalArgumentException("Image compression failed")
-//
-//        val filePart = MultipartBody.Part.createFormData(
-//            "file",
-//            "image.jpg",
-//            imageData.toRequestBody("image/jpeg".toMediaTypeOrNull())
-//        )
-//
-//        val descriptionPart = item.description.toRequestBody("text/plain".toMediaTypeOrNull())
-//        val spoilerPart = (if (item.spoiler) "true" else "false").toRequestBody("text/plain".toMediaTypeOrNull())
-//
-//        val imageDTO = api.uploadPostImage(filePart, descriptionPart, spoilerPart)
-//        return imageDTO.toModel() ?: throw IllegalStateException("DTO to Model conversion failed")
-//    }
+        val imageData = item.image.compressForUpload(10.0)
+            ?: throw IllegalArgumentException("Image compression failed")
+
+        val filePart = MultipartBody.Part.createFormData(
+            "file",
+            "image.jpg",
+            imageData.toRequestBody("image/jpeg".toMediaTypeOrNull())
+        )
+
+        val descriptionPart = item.description.toRequestBody("text/plain".toMediaTypeOrNull())
+        val spoilerPart = (if (item.spoiler) "true" else "false").toRequestBody("text/plain".toMediaTypeOrNull())
+
+        val imageDTO = api.uploadPostImage(filePart, descriptionPart, spoilerPart)
+        return imageDTO.toModel() ?: throw IllegalStateException("DTO to Model conversion failed")
     }
 }
