9b1715cb55ec92603194d38ee902ec16b9fff5d4
Inha Yoon <yih2776@gmail.com>
2025-10-04 23:05:06 +0900

    fix: Course->Lecture

--- app/src/main/java/com/example/soap/Domain/Models/OTL/LectureReview.kt
+++ app/src/main/java/com/example/soap/Domain/Models/OTL/LectureReview.kt
@@ -5,9 +5,27 @@ data class LectureReview(
     val course: Course,
     val lecture: Lecture,
     val content: String,
-    val like: Int,
+    var like: Int,
     val grade: Int,
     val load: Int,
     val speech: Int,
-    val isLiked: Boolean
-)
+    var isDeleted: Boolean,
+    var isLiked: Boolean
+) {
+
+    companion object {
+        val letters = listOf("?", "F", "D", "C", "B", "A")
+    }
+
+    // Letter grade for the grade
+    val gradeLetter: String
+        get() = if (grade in letters.indices) letters[grade] else "?"
+
+    // Letter grade for the load
+    val loadLetter: String
+        get() = if (load in letters.indices) letters[load] else "?"
+
+    // Letter grade for the speech
+    val speechLetter: String
+        get() = if (speech in letters.indices) letters[speech] else "?"
+}
