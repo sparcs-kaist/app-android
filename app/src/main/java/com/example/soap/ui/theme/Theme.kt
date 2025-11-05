e5e4291e1afe7049275eaec021b6e5f4b6629608
Inha Yoon <yih2776@gmail.com>
2025-11-05 15:24:23 +0900

    fix: screen blinkering problem (darkmode)

--- app/src/main/java/com/example/soap/ui/theme/Theme.kt
+++ app/src/main/java/com/example/soap/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.example.soap.ui.theme
 
 import android.app.Activity
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
@@ -12,6 +13,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowInsetsControllerCompat
@@ -138,12 +140,20 @@ fun Theme(
 
     val view = LocalView.current
     if (!view.isInEditMode) {
+        val background = colorScheme.background
+
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.setBackgroundDrawable(ColorDrawable(background.toArgb()))
+        }
+
         SideEffect {
             val window = (view.context as Activity).window
             val insetsController = WindowInsetsControllerCompat(window, window.decorView)
 
             insetsController.isAppearanceLightStatusBars = !darkTheme
             insetsController.isAppearanceLightNavigationBars = !darkTheme
+            window.setBackgroundDrawable(ColorDrawable(colorScheme.background.toArgb()))
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 window.navigationBarColor = if (darkTheme) {
