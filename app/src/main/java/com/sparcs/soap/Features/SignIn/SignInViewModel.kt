d54dc8dfe21e38e4d6991a846e950f2dc116872e
Inha Yoon <yih2776@gmail.com>
2025-11-27 05:45:12 +0900

    fix: auth login error catch

--- app/src/main/java/com/sparcs/soap/Features/SignIn/SignInViewModel.kt
+++ app/src/main/java/com/sparcs/soap/Features/SignIn/SignInViewModel.kt
@@ -1,21 +1,22 @@
 package com.sparcs.soap.Features.SignIn
 
 import android.app.Activity
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.sparcs.soap.Domain.Enums.Auth.AuthUseCaseError
 import com.sparcs.soap.Domain.Usecases.AuthUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.coroutines.cancellation.CancellationException
 
 interface SignInViewModelProtocol {
     var isLoading: Boolean
     var errorMessage: String?
 
-    fun signIn(activity: Activity)
+    suspend fun signIn(activity: Activity)
 }
 
 @HiltViewModel
@@ -24,23 +25,27 @@ class SignInViewModel @Inject constructor(
 ) : ViewModel(), SignInViewModelProtocol {
 
     override var isLoading by mutableStateOf(false)
-
     override var errorMessage by mutableStateOf<String?>(null)
 
-    override fun signIn(
-        activity: Activity
-    ) {
+    override suspend fun signIn(activity: Activity) {
         isLoading = true
         errorMessage = null
-
-        viewModelScope.launch {
-            try {
-                authUseCase.signIn(activity)
-            } catch (e: Exception) {
-                errorMessage = e.message
-            } finally {
-                isLoading = false
-            }
+        try {
+            authUseCase.signIn(activity)
+        } catch (e: CancellationException) {
+            errorMessage = "User cancelled"
+            Log.e("SignInViewModel", "Sign in cancelled: $e")
+            throw e
+        } catch (e: AuthUseCaseError) {
+            errorMessage = e.message
+            Log.e("SignInViewModel", "Failed to sign in: $e")
+            throw e
+        } catch (e: Exception) {
+            errorMessage = e.message
+            Log.e("SignInViewModel", "Unexpected error: $e")
+            throw e
+        } finally {
+            isLoading = false
         }
     }
 }
