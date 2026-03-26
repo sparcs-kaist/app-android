272caaf4d475ddc9fad385ce87bd028dd528c84b
mbp16 <phw1508@naver.com>
2026-03-27 04:09:30 +0900

    feat: add buddy watch module

---
+++ buddywatch/src/main/java/org/sparcs/soap/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package org.sparcs.soap.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun SoapTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
