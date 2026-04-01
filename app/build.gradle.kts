2cee1335d50f1cf4334c178738d082e992e2efdc
Inha Yoon <yih2776@gmail.com>
2026-04-02 00:31:00 +0900

    chore: bump version and update taxi room joining logic
    * chore: bump `versionCode` to 17 in `app` and 14 in `buddywatch`
    * fix: update `TaxiPreviewView` to skip joining logic if the user is already a participant in the room
    * feat: remove `AD_ID` permission from `AndroidManifest.xml` explicitly via `tools:node="remove"`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "org.sparcs.soap"
         minSdk = 31
         targetSdk = 35
-        versionCode = 13
+        versionCode = 17
         versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
